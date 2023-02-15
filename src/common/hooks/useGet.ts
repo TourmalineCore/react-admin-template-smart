6db35bba9d5f33b1858f6a93a293f15dcc0a52fa
Katariniss <echekina@gmail.com>
2023-02-15 17:42:05 +0500

    chore: add httpService and useGet hook

---
+++ src/common/hooks/useGet.ts
@@ -0,0 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
+import { AxiosRequestConfig } from 'axios';
+import { useErrorHandler } from 'react-error-boundary';
+
+import { api } from '../utils/HttpClient';
+
+export const useGet = <Type = any>({
+  url,
+  config,
+  errorMessage = 'Error',
+}:{
+  url: string;
+  config?:AxiosRequestConfig<Type>,
+  errorMessage?: string;
+}) => {
+  const { isLoading, data, error } = useQuery({
+    queryKey: ['name'],
+    queryFn: () => api.get<Type>(url, config)
+      .then((response) => response.data)
+      .catch((err) => { throw new Error(errorMessage, { cause: err }); }),
+  });
+
+  useErrorHandler(error);
+
+  return { isLoading, data };
+};
--- src/pages/EmployeePage/section/Name/Name.tsx
+++ src/pages/EmployeePage/section/Name/Name.tsx
@@ -1,28 +1,21 @@
-import { useQuery } from '@tanstack/react-query';
-import { useErrorHandler } from 'react-error-boundary';
-import Skeleton from 'react-loading-skeleton';
 import { useSearchParams } from 'react-router-dom';
+import Skeleton from 'react-loading-skeleton';
 import customErrorBoundary from '../../../../common/error-handling/errorBoundary';
-// import errorBoundary from '../../../../common/error-handling/errorBoundary';
-import { Service } from '../../../../common/utils/Services';
-// import ErrorComponent from '../../../../components/ErrorComponent/ErrorComponent';
+
+import { useGet } from '../../../../common/hooks/useGet';
 
 function Name() {
   const [params] = useSearchParams();
-
   const id = params.get('id');
 
-  const { isLoading, data, error } = useQuery({
-    queryKey: ['name'],
-    queryFn: () => Service.getName(id),
-
+  const { isLoading, data } = useGet({
+    url: `users/${id}`,
+    errorMessage: 'Error get name',
   });
-
-  useErrorHandler(error);
 
   return (
     <div className="section name">
-      {data}
+      {data?.name}
       {' '}
       {isLoading && (<Skeleton />)}
     </div>
