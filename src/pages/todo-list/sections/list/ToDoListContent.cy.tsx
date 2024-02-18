ef7d613a451eecd7cc072c84422336cae8970521
Aleksandr Shinkarev <ashinkarev@tourmalinecore.com>
2024-02-18 13:38:07 +0500

    test(todo): ToDoListContent.cy.tsx
      GIVEN Two ToDo items
      WHEN render the component
      SHOULD see them

---
+++ src/pages/todo-list/sections/list/ToDoListContent.cy.tsx
@@ -0,0 +1,12 @@
+describe(`ToDoListContent`, () => {
+  it(`
+  GIVEN Two ToDo items
+  WHEN render the component
+  SHOULD see them
+  `, () => {
+    cy.mount(<ToDoListContent />);
+
+    cy.contains(`First ToDo`);
+    cy.contains(`Second ToDo`);
+  });
+});
