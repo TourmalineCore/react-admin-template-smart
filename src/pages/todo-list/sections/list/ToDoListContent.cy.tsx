a912bf20db38d9167554ddc49edc0a4cab329a52
Aleksandr Shinkarev <ashinkarev@tourmalinecore.com>
2024-02-18 14:35:06 +0500

    test: ToDoListContent.cy.tsx
      GIVEN default state with no ToDo items
      WHEN two ToDo items for initialization of state
      SHOULD see them

--- src/pages/todo-list/sections/list/ToDoListContent.cy.tsx
+++ src/pages/todo-list/sections/list/ToDoListContent.cy.tsx
@@ -8,25 +8,57 @@ describe(`ToDoListContent`, () => {
   WHEN render the component
   SHOULD see them
   `, () => {
-    mountComponent();
+    mountComponent({
+      todos: [
+        {
+          name: `First Fizz`,
+        },
+        {
+          name: `Second Buzz`,
+        },
+      ],
+    });
 
     cy.contains(`First Fizz`);
     cy.contains(`Second Buzz`);
   });
+
+  it(`
+  GIVEN default state with no ToDo items
+  WHEN two ToDo items for initialization of state
+  SHOULD see them
+  `, () => {
+    mountComponent({
+      todos: [],
+    });
+
+    cy.get<ToDoListState>(`@toDoListState`)
+      .then((toDoListState) => {
+        toDoListState.initialize({
+          todos: [
+            {
+              name: `Third`,
+            },
+            {
+              name: `Forth`,
+            },
+          ],
+        });
+      });
+
+    cy.contains(`Third`);
+    cy.contains(`Forth`);
+  });
 });
 
-function mountComponent() {
+function mountComponent({
+  todos,
+}) {
   const toDoListState = new ToDoListState();
+  cy.wrap(toDoListState).as(`toDoListState`);
 
   toDoListState.initialize({
-    todos: [
-      {
-        name: `First Fizz`,
-      },
-      {
-        name: `Second Buzz`,
-      },
-    ],
+    todos,
   });
 
   cy.mount(
