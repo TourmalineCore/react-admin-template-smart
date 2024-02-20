70fdedd23d21480303269408688a3e6ef32e2326
Aleksandr Shinkarev <ashinkarev@tourmalinecore.com>
2024-02-20 17:21:34 +0500

    test: ToDoListContainer.cy.tsx
      GIVEN Two ToDo items from network
      WHEN render the component and there is nothing selected
      SHOULD see disabled Delete button

--- src/pages/todo-list/sections/list/ToDoListContainer.cy.tsx
+++ src/pages/todo-list/sections/list/ToDoListContainer.cy.tsx
@@ -32,6 +32,34 @@ describe(`ToDoListContainer`, () => {
 
     cy.contains(`Deux`);
   });
+
+  it(`
+  GIVEN Two ToDo items from network
+  WHEN render the component and there is nothing selected
+  SHOULD see disabled Delete button
+  `, () => {
+    cy.intercept(
+      `GET`,
+      `${API_ROOT}/todos`,
+      {
+        todos: [
+          {
+            id: 1,
+            name: `Un`,
+          },
+          {
+            id: 2,
+            name: `Deux`,
+          },
+        ],
+      },
+    );
+
+    mountComponent();
+
+    cy.get(`[data-cy=delete-selected-todos-button]`)
+      .should(`be.disabled`);
+  });
 });
 
 function mountComponent() {
