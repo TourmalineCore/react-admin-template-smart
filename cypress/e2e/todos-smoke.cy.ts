8aa936424a37a56e8f8ffc61934c9cae6d6d22ae
Aleksandr Shinkarev <ashinkarev@tourmalinecore.com>
2024-02-20 20:01:55 +0500

    test: smoke to test add

--- cypress/e2e/todos-smoke.cy.ts
+++ cypress/e2e/todos-smoke.cy.ts
@@ -8,6 +8,25 @@ describe(`ToDo List Smoke`, () => {
 
     cy.get(`[data-cy="todo-item"]`).should(`have.length.greaterThan`, 0);
   });
+
+  it(`
+  GIVEN ToDo items page
+  WHEN add a new ToDo
+  SHOULD see it in the list
+  `, () => {
+    cy.visit(`/todos`);
+
+    const newToDoName = `[E2E-SMOKE] ${new Date()}`;
+
+    cy.get(`[data-cy="new-todo-name-input"]`)
+      .type(newToDoName);
+
+    cy.get(`[data-cy="add-new-todo-button"]`)
+      .click();
+
+    cy.get(`[data-cy="todos"]`)
+      .contains(newToDoName);
+  });
 });
 
 export {};
