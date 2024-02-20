33cc052ff5cc080324976d184585b1f366638479
Aleksandr Shinkarev <ashinkarev@tourmalinecore.com>
2024-02-20 17:01:25 +0500

    feat: ToDoListContent.cy.tsx
      GIVEN two ToDo items
      WHEN click on the 2nd of them
      SHOULD add its ID to the list of selected IDs

--- src/pages/todo-list/sections/list/ToDoListContent.tsx
+++ src/pages/todo-list/sections/list/ToDoListContent.tsx
@@ -18,6 +18,12 @@ const ToDoListContent = observer(() => {
               key={`todo-${id}`}
               data-cy="todo-item"
             >
+              <input
+                type="checkbox"
+                data-cy="todo-checkbox"
+                onChange={() => toDoListState.toggleToDoIdSelection({ toDoId: id })}
+                checked={toDoListState.selectedToDoIds.includes(id)}
+              />
               {name}
             </li>
           ))
