e33bd7efe23aa75cb3ff67c1af7a4dd6369a3c22
Aleksandr Shinkarev <ashinkarev@tourmalinecore.com>
2024-02-21 13:57:22 +0500

    fix

--- src/pages/todo-list/sections/list/state/ToDoListStateContext.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-import { ToDoListState } from "./ToDoListState";
-
-const ToDoListStateContext = createContext<ToDoListState>(null as unknown as ToDoListState);
-
-export {
-  ToDoListStateContext,
-};
